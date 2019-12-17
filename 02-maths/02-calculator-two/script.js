2179e1259b4c95c4490566205971f673a6398f41
imane <benbenimane@hotmail.com>
2019-12-17 12:29:04 +0100

    calculator adjustments

--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -1,34 +1,24 @@
-/* becode/javascript
- *
- * /02-maths/02-calculator-two/script.js - 2.2: calculatrice (2)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
-function calc('this.id') {
+function calc(sali) {
     let number1 = document.getElementById ("op-one" ).value;
     let number2 = document.getElementById ("op-two" ).value ;
+    let result;
 
-
-    switch (calc()){
+    switch (sali){
         case 'addition':
-            let result = parseInt(number1) + parseInt(number2);
+            result = parseInt(number1) + parseInt(number2);
             alert (result);
             break;
         case 'substraction':
-            let result = parseInt(number1) - parseInt(number2);
+            result = parseInt(number1) - parseInt(number2);
             alert (result);
             break;
         case 'multiplication':
-            let result = parseInt(number1) - parseInt(number2);
+           result = parseInt(number1) * parseInt(number2);
             alert (result);
             break;
         case 'division':
-            let result = parseInt(number1) - parseInt(number2);
+            result = parseInt(number1) / parseInt(number2);
             alert (result);
             break;
     }
@@ -36,5 +26,3 @@ function calc('this.id') {
 
 
 }
-
-
