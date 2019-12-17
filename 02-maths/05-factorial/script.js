66b30b499c28adbd4158118ea4a66f49f015a56f
imane <benbenimane@hotmail.com>
2019-12-17 17:17:39 +0100

    started solving the factorial thing, i think i am almost there but something is missing

--- 02-maths/05-factorial/script.js
+++ 02-maths/05-factorial/script.js
@@ -1,22 +1,30 @@
-/* becode/javascript
- *
- * /02-maths/05-factorial/script.js - 2.5: Factorielle
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
     
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", function() {
 
-        // your code here
+        let input = document.getElementById('number').value;
+
+        let number = parseInt(input);
+
+            console.log(number);
+            function factorial(number){
+            if (number > 0 && number <= 1){
+
+                return 1;
+            }
+
+            else {
+
+                return number * factorial(number-1);
+            }
+
+          }
+
+        document.getElementById('number').value; = number ;
 
     });
 
-})();
+
