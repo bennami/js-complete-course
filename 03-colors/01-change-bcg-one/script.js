e6b4852788fe672b6e226d069b95e8b796fc3b1b
imane <benbenimane@hotmail.com>
2019-12-18 12:23:13 +0100

    change bgcolor with switch

--- 03-colors/01-change-bcg-one/script.js
+++ 03-colors/01-change-bcg-one/script.js
@@ -1,10 +1,23 @@
 
-document.getElementById("red").addEventListener("click", function() {
+function bgcolor(change){
 
-   document.getElementById('body').innerHTML; function(){
+    switch (change){
 
+        case 'red':
+
+            document.body.style.backgroundColor = 'red';
+        break;
+
+        case 'green':
+            document.body.style.backgroundColor = 'green';
+            break;
+
+        case 'blue':
+            document.body.style.backgroundColor = 'blue';
+            break;
+
+        case 'yellow':
+            document.body.style.backgroundColor = 'yellow';
+            break;
     }
-
-
-
 }
