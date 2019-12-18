dbad966615ebbba4116b2395b56a3f139867344e
imane <benbenimane@hotmail.com>
2019-12-18 11:18:45 +0100

    fizzbuzz solved

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -2,10 +2,31 @@
 
 (function fizzbuzz() {
 
-    let n = 2;
 
-    while( n >1 && n <100){
-      return (n % 3);
+
+    for ( let i = 1; i <= 100; i ++){
+
+        if ( (i % 3 === 0) && (i % 5 === 0) ) {
+
+            console.log(i + " Fizzbuzz");
+        }
+         else if (i % 3 === 0){
+
+            console.log ( i + " fizz");
+        }
+
+        else if ( i % 5 === 0 ){
+
+            console.log( i + " buzz");
+        }
+
+
+        else {
+            console.log (i);
+        }
+
     }
-console.log(fizzbuzz);
+
+    // there are better ways to fix this.
+
 })();
