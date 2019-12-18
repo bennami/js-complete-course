65d5a7ae3f132e1841d90c2eaefe8b7488d9abce
imane <benbenimane@hotmail.com>
2019-12-18 15:49:57 +0100

    solved the random color  generator

--- 03-colors/03-change-bcg-three/script.js
+++ 03-colors/03-change-bcg-three/script.js
@@ -1,16 +1,29 @@
-/* becode/javascript
- *
- * /03-colors/03-change-bcg-three/script.js - 3.3: couleur de fond (3)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
 
-    // your code here
+    document.getElementById('run').addEventListener("click", function () {
+
+        //generate random color
+
+let r, g, b;
+
+        //change bg color
+r = Math.floor(Math.random() * 256);
+g = Math.floor(Math.random() * 256);
+b = Math.floor(Math.random() * 256);
+
+let color;
+
+color = "rgb(" + r + ","+ g + "," + b + ")";
+
+
+
+document.body.style.backgroundColor =  color;
+console.log(color);
+
+    });
+
+
 
 })();
