dc72941b45e011881548665e28d77b2d2022ec30
imane <benbenimane@hotmail.com>
2019-12-18 13:05:25 +0100

    take input and change bg color

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -1,16 +1,16 @@
-/* becode/javascript
- *
- * /03-colors/02-change-bcg-two/script.js - 3.2: couleur de fond (2)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+(function () {
+//get id and listen to a click
+    document.getElementById('run').addEventListener("click", function () {
 
-(function() {
+    let input;
 
-    // your code here
+    input = document.getElementById('color').value;
+
+//use input as bg color
+    document.body.style.backgroundColor = input;
+
+    console.log (input);
+    });
 
 })();
