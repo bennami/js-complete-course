ab3525e1029504723044b7505a05d34ad96a2f3e
imane <benbenimane@hotmail.com>
2019-12-19 17:00:53 +0100

    started on 4.5,last commit, 4.3 still not solved

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -1,16 +1,18 @@
-/* becode/javascript
- *
- * /04-dates/05-get-spooky-fridays/script.js - 4.5: calcul des vendredi 13
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
+    document.getElementById("run").addEventListener("click", function() {
 
+
+        let yearInput = document.getElementById('year').value;
+        console.log(yearInput);
+
+        function check( yearInput) {
+
+            for(yearinput)
+
+        }
+
+    });
     // your code here
 
 })();
