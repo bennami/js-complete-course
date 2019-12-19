0c9a119a078dc6e16037e7af87236c2978215433
imane <benbenimane@hotmail.com>
2019-12-19 11:47:12 +0100

    figuring out what data i need to calculate age

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -1,16 +1,34 @@
-/* becode/javascript
- *
- * /04-dates/03-age-by-select/script.js - 4.3: calculateur d'âge
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
+(function () {
+    document.getElementById("run").addEventListener("click", function() {
 
-    // your code here
+        let day = document.getElementById("dob-day").value;
+        console.log(day);
+
+        let month = document.getElementById("dob-month").value;
+        console.log(month);
+
+        let year = document.getElementById("dob-year").value;
+        console.log (year);
+
+        let bday = day + " " + month + " " + year ;
+        console.log(bday) ;
+
+
+        let currentDate = new Date();
+        CurrentDay = currentDate.getDay();
+        CurrentMonth = currentDate.getMonth();
+        CurrentYear = currentDate.getFullYear();
+        console.log(CurrentYear);
+
+
+
+    });
+
+
+
+
+
 
 })();
