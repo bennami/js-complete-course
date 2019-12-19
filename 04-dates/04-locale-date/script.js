f022617af6d8753ac453d602fc53f14b55be87ad
imane <benbenimane@hotmail.com>
2019-12-19 15:36:03 +0100

    4.4 still need to assing arr string to value of variable, jumped to 4.5

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -5,6 +5,25 @@
 
    let date = new Date(); //stores current date and time in variable
 
+    let dayname = date.getDay();
+    let day =date.getDate();
+
+
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+        let time = hour +":"+ minutes;
+
+    ArrDays = [ 'Monday', 'Tuesday', 'wednesday', 'thursday', 'friday'];
+
+    ArrMonths = ['January', 'February', 'March', 'April', 'May',' June', 'July', 'August','September', 'October', 'November', 'December'];
+
+
+    console.log(dayname, day ,month, year,  time);
+
+
     document.getElementById("target").innerHTML = date;
 
   //  date.toLocaleDateString()
