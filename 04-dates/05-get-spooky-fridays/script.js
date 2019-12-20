ba8311261107ea9a15a0d419879df93ba5ed7b17
imane <benbenimane@hotmail.com>
2019-12-20 10:49:48 +0100

    solved, result in two alert windows, to solve later, nice to have

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -1,16 +1,37 @@
 
 (function() {
     document.getElementById("run").addEventListener("click", function() {
-
-
         let yearInput = document.getElementById('year').value;
-        console.log(yearInput);
+       // console.log(yearInput);
+        let date = new Date(yearInput, );
+       // console.log(date);
 
-        function check( yearInput) {
+function luckyFriday(yearInput){
 
-            for(yearinput)
+
+        for ( month=0; month<12; month ++){
+let result = " "
+            var d = new Date(yearInput, month,13 );
+
+            if (d.getDay() === 5){
+
+                monthArr =['January ', 'February ', 'March ', 'April ', 'May ',' June ', 'July ', 'August ','September ', 'October ', 'November ', 'December '] ;
+
+           alert(monthArr[month]);
+
+            }
 
         }
+
+
+}
+
+luckyFriday(yearInput);
+
+
+
+
+//get year, get dates
 
     });
     // your code here
