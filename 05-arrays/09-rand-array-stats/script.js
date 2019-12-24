415eb9316379b77072e04162821f554141de8c8d
imane <benbenimane@hotmail.com>
2019-12-24 09:50:06 +0100

    added ways to solve reduce and solved average

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -17,17 +17,43 @@
 
         }
 
-      let min = Math.min.apply(null, arr); //why .apply and null??????
-
+      let min;
+        min = Math.min.apply(null, arr); //why .apply and null??????
         document.getElementById('min').innerHTML = min ;
 
 
-       let max = Math.max.apply(null, arr);
+       let max;
+        max = Math.max.apply(null, arr);
         document.getElementById('max').innerHTML = max;
 
-        let sum = arr.reduce((acc,arr) => acc +arr);
-        console.log(sum)
+        let sum; //accumulator takes first value of array if not defined
+        sum = arr.reduce((acc,arr) => acc +arr);
         document.getElementById('sum').innerText = sum;
+
+        //another way to reduce with writing function full out
+        function answer (acc, arr){
+            return acc + arr;
+        }
+        let redu = arr.reduce(answer);
+        console.log(redu);
+
+        //with no reduce
+        let total = 0;
+        for(let i =0; i < arr.length; i++){
+
+            total += arr[i];
+        }
+         console.log(total);
+
+        //average
+
+        let avg = (sum /arr.length);
+        console.log(avg);
+        document.getElementById('average').innerHTML = avg ;
+
+
+
+
 
 
 
