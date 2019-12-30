5001818375e6e6ca6a8a8154608e149d023c158c
imane <benbenimane@hotmail.com>
2019-12-30 09:19:54 +0100

    6.3 solved with a for loop

--- 06-dom/03-select-three/script.js
+++ 06-dom/03-select-three/script.js
@@ -1,16 +1,13 @@
-/* becode/javascript
- *
- * /06-dom/03-select-three/script.js - 6.3: sélection multiple par sélecteur css
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
 
-    // your code here
+   let x =  document.querySelectorAll(".target");
+
+    let i;
+    for(i=0; i< x.length; i++){
+        x[i].innerHTML= "owned";
+    }
+
 
 })();
