711891f3942e5cd442048ebba0dc75ddc0a9edf4
imane <benbenimane@hotmail.com>
2019-12-30 10:24:44 +0100

    6.4 solved

--- 06-dom/04-attr-create/script.js
+++ 06-dom/04-attr-create/script.js
@@ -1,16 +1,11 @@
-/* becode/javascript
- *
- * /06-dom/04-attr-create/script.js - 6.4: manipulation d'attributs & création d'élément
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
+    let data = document.getElementById('source').getAttribute('data-image');
 
-    // your code here
+    let newimg = document.createElement( 'img');
+    newimg.src = data;
+    document.getElementById("target").append(newimg);
 
+    document.getElementById('source').remove();
 })();
