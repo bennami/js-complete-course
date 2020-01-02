ff06384056469a4e25537888cecb8510dad0b9d7
imane <benbenimane@hotmail.com>
2020-01-02 09:49:25 +0100

    trying to use a for loop to change foontsize, or rather increase and decrease

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -1,13 +1,23 @@
 
 
 (function() {
+//create emptty array and get text from html
     let arr = [];
-    let text =document.getElementById('target').innerHTML;
-    console.log(text);
+    let text = document.getElementById('target').innerHTML;
 
-arr.push(text);
+//push text into array
+    arr.push(text);
+//console.log(arr);
+// split to make string of every character in text
+let arrnew = text.split("");
+    //console.log(arrnew);
+//loop through string to change font
+  let i=0;
+  let newsize =  document.getElementById('target').style.fontSize;
+for (i ; i < arrnew.length; i++){
 
-console.log(arr);
-    // your code here
+     newsize++;
 
+}
+    console.log(arrnew[i]);
 })();
