53cbe5ae99d6f65bc958db0b9ace86aeaf74aeca
imane <benbenimane@hotmail.com>
2020-01-06 12:10:04 +0100

    7.4 if numbers reach max value it goes back to original value

--- 07-misc/04-worst-ui-two/script.js
+++ 07-misc/04-worst-ui-two/script.js
@@ -2,7 +2,6 @@
 
 (function() {
 
-
     //get html button
     let first = document.getElementById('part-one');
     let second = document.getElementById('part-two');
@@ -16,63 +15,72 @@
     //concat result in a var
     let total =  '0' + first.innerHTML + second.innerHTML + third.innerHTML + four.innerHTML ;
 
-    //assign total to output tag
- //  total = output.innerHTML;
 
-    console.log(first,second,third,four);
+    // Create event listener for each button
+    document.getElementById('part-one').addEventListener('click',function () {
 
-    console.log(total);
-document.getElementById('part-one').addEventListener('click',function () {
 
-     first.innerHTML++;
+     if(first.innerHTML< 499){
+         first.innerHTML++;
+     }else{
+         first.innerHTML= "460";
+     }
+     //increment value, place it back to output and return value
      output.innerHTML = '0' + first.innerHTML + second.innerHTML + third.innerHTML + four.innerHTML;
-return total;
+     return total;
 
-
-});
+    });
 
     document.getElementById('part-two').addEventListener("click", function(){
 
 
-        second.innerHTML++;
+        if(second.innerHTML < 99){
+            second.innerHTML++;
+        }else{
+            second.innerHTML= "0";
+        }
+        //increment value, put zero for value 0-9, place it back to output and return value
         if(second.innerHTML<10){
             second.innerHTML =  "0" + second.innerHTML;
         }
         output.innerHTML = '0' + first.innerHTML + second.innerHTML + third.innerHTML + four.innerHTML ;
-
-return total;
+        return total;
 
     });
 
     document.getElementById('part-three').addEventListener("click", function(){
 
-       third.innerHTML++;
 
+
+       if(third.innerHTML < 99){
+           third.innerHTML++;
+       }else{
+           third.innerHTML= "0";
+       }
+        //increment value, put zero for value 0-9, place it back to output and return value
         if(third.innerHTML <10){
           third.innerHTML =  "0" + third.innerHTML;
         }
-      output.innerHTML = '0' + first.innerHTML + second.innerHTML + third.innerHTML + four.innerHTML ;
-
-       return total;
-
-
-
+        output.innerHTML = '0' + first.innerHTML + second.innerHTML + third.innerHTML + four.innerHTML ;
+        return total;
 
     });
 
     document.getElementById('part-four').addEventListener("click", function(){
 
 
+        if(four.innerHTML < 99){
         four.innerHTML++;
+        }else{
+            four.innerHTML= "0";
+        }
+        //increment value, put zero for value 0-9, place it back to output and return value
         if(four.innerHTML <10){
             four.innerHTML =  "0" +  four.innerHTML;
         }
-
         output.innerHTML = '0' + first.innerHTML + second.innerHTML + third.innerHTML + four.innerHTML ;
-
-
+        return total;
     });
-
 
 
 /*
@@ -122,6 +130,8 @@ calc();
 */
 
 /*
+
+
 // for each
 
 
@@ -154,4 +164,6 @@ calc();
         })
 
 */
-    })();
+
+
+})();
