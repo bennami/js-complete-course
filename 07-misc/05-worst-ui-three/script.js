0d9912daebad90be8a1eb0d45615e5b80c882a30
imane <benbenimane@hotmail.com>
2020-01-06 14:50:31 +0100

    7.5 solved but could be better, not using min and max data for 2-4

--- 07-misc/05-worst-ui-three/script.js
+++ 07-misc/05-worst-ui-three/script.js
@@ -5,32 +5,57 @@
    let two = document.getElementById('part-two');
    let three = document.getElementById('part-three');
    let four = document.getElementById('part-four');
+   let result =  document.getElementById('target');
+
+    let calc1 = '00';
+    let calc2 = '00';
+    let calc3 = '00';
+    let calc4 = '00';
+
 
 
 
     document.getElementById('fix-part-one').addEventListener("click", function(){
-let calc1= (Math.floor(Math.random()*((one.getAttribute('data-max')-one.getAttribute('data-min'))+1)+460));
-console.log(calc1);
 
-document.getElementById('part-one').value = calc1;
-return calc1;
+        calc1= (Math.floor(Math.random()*((one.getAttribute('data-max')-one.getAttribute('data-min'))+1)+460));
+        document.getElementById('part-one').value = calc1;
+        calc1 = '0'+ calc1;
+        result.innerHTML = calc1 + calc2 + calc3 + calc4;
+        return calc1;
 
 
     });
 
     document.getElementById('fix-part-two').addEventListener("click", function(){
-        let calc2= (Math.floor(Math.random()*((one.getAttribute('data-max')-one.getAttribute('data-min')))+99));
-        console.log(calc2);
+
+         calc2= (Math.floor(Math.random()*99)+1);
+        if(calc2< 10){
+            calc2 = "0"+calc2;
+        }
+        document.getElementById('part-two').value = calc2;
+        result.innerHTML = calc1 + calc2 + calc3 + calc4;
+        return calc2;
     });
 
     document.getElementById('fix-part-three').addEventListener("click", function(){
-        let calc3= (Math.floor(Math.random()*((one.getAttribute('data-max')-one.getAttribute('data-min')))+99));
 
-        console.log(calc3);
+         calc3= (Math.floor(Math.random()*99)+1);
+        if(calc3< 10){
+            calc3 = "0"+calc3;
+        }
+        document.getElementById('part-three').value = calc3;
+        result.innerHTML = calc1 + calc2 + calc3 + calc4;
+        return calc3;
     });
 
     document.getElementById('fix-part-four').addEventListener("click", function(){
-        let calc4= (Math.floor(Math.random()*((one.getAttribute('data-max')-one.getAttribute('data-min')))+99));
-        console.log(calc4);
+
+         calc4= (Math.floor(Math.random()*99)+1);
+        if(calc4< 10){
+            calc4 = "0"+calc4;
+        }
+        document.getElementById('part-four').value = calc4;
+        result.innerHTML = calc1 + calc2 + calc3 + calc4;
+        return calc4;
     });
 })();
