8f69b926c63be1401d37c3d9bff2f3c1b08c34ae
imane <benbenimane@hotmail.com>
2020-01-07 10:33:31 +0100

    had to assign it before click event so it gets assigned on load, solved

--- 07-misc/07-storage-clicker/script.js
+++ 07-misc/07-storage-clicker/script.js
@@ -1,18 +1,19 @@
 
 
 (function() {
+    //define before click function so it gets the value on page load and refresh
+    document.getElementById('target').innerHTML = localStorage.getItem('number');
 
+    //eventlistener for button to execute increment
+    document.getElementById("increment").onclick = function() {increment()};
 
-
-    document.getElementById("increment").onclick = function() {getvalue()};
-
-    let count = 1;
-    function getvalue(){
+    let count = localStorage.getItem('number');
+    function increment(){
 
         //save value in localstorage
         localStorage.setItem('number', count++);
 
-        //assign it to p tag
+        //assign increment to target
         document.getElementById('target').innerHTML = localStorage.getItem('number');
 
     }
