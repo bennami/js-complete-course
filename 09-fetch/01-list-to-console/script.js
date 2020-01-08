5cde7fff27e0dd1374cc1a3272c99a92be061106
imane <benbenimane@hotmail.com>
2020-01-08 09:52:34 +0100

    9.1 solved following code train tutorial

--- 09-fetch/01-list-to-console/script.js
+++ 09-fetch/01-list-to-console/script.js
@@ -1,11 +1,41 @@
 
+
+
 (() => {
 
+    document.getElementById("run").onclick = function() {catchXmen()};
+
+//solution without async finction
+
+function hey() {
+
+    fetch('http://localhost:5000/_shared/api.json').then( response => {
+
+    return response.json();
+
+    })
+        .then(content =>{
+
+    console.log(content);
+
+    })
+
+
+    }
+
+// the async function way
+    async function catchXmen() {
+
+    const  response = await fetch('http://localhost:5000/_shared/api.json ');
+    const json = await response.json();
+    console.log(json);
+
+    }
 
 
 
 
-
+//https://www.youtube.com/watch?v=tc8DU14qX6I
 
 
 
