ed9831ea624606bb90408563d8a73edc24707327
imane <benbenimane@hotmail.com>
2020-01-08 17:00:47 +0100

    9.2 solved, 9.3 not yet

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -1,14 +1,46 @@
-/* becode/javascript
- *
- * /09-fetch/02-list-to-template/script.js - 11.2: liste vers template
- *
- * coded by leny@BeCode
- * started at 12/05/2019
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    //create var for URL
+    const Url = 'http://localhost:5000/_shared/api.json';
+
+    //create function to fetch data
+    async function Getdata() {
+
+        //fetch stream of data
+        let response = await fetch(Url);
+
+        //convert it to json format
+        let data = await response.json();
+
+
+        //loop to get every object for every hero
+        for(i=0; i < 5; i++) {
+
+            console.log( data.heroes[i].id);
+            console.log( data.heroes[i].name);
+            console.log( data.heroes[i].alterEgo);
+            console.log( data.heroes[i].abilities);
+
+        //get the tags of tpl-hero and put it in a variable
+        let template =  document.getElementById('tpl-hero') ;
+
+        //create a copy of the html tags inside tpl-hero;
+        let template2 = template.content.cloneNode(true);
+
+        //put object values inside according .class tag
+        template2.querySelector('.name').innerHTML = data.heroes[i].name;
+        template2.querySelector('.alter-ego').innerHTML = data.heroes[i].alterEgo;
+        template2.querySelector('.powers').innerHTML = data.heroes[i].abilities;
+
+        //append template to target
+        document.getElementById('target').appendChild(template2);
+
+        }
+    }
+
+    //click event to run function
+    document.getElementById("run").onclick = function() {Getdata()};
+
 })();
--- 09-fetch/03-details/script.js
+++ 09-fetch/03-details/script.js
@@ -1,14 +1,36 @@
-/* becode/javascript
- *
- * /09-fetch/03-details/script.js - 11.3: details
- *
- * coded by leny@BeCode
- * started at 12/05/2019
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    //create var for URL
+    const Url = 'http://localhost:5000/_shared/api.json';
+
+    //create var for scope
+
+
+    //create function to fetch data
+    async function Getdata() {
+
+        //fetch stream of data
+        let response = await fetch(Url);
+
+        //convert it to json format
+       let data = await response.json();
+
+
+        for(i=0; i < 5; i++) {
+
+
+            console.log( data.heroes[i].name);
+
+
+        }
+
+
+
+    }
+
+    //clcik event to run function
+    document.getElementById("run").onclick = function() {Getdata()};
+
 })();
