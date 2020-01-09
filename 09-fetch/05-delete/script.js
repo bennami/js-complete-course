8744ffd858f424e4f3eaeb20a688d7464d072ef7
imane <benbenimane@hotmail.com>
2020-01-09 15:20:18 +0100

    steven debugged, it is better to use a index function so it keeps deleting objects

--- 09-fetch/05-delete/script.js
+++ 09-fetch/05-delete/script.js
@@ -5,8 +5,6 @@
     //create var for URL
     const Url = 'http://localhost:5000/_shared/api.json';
 
-
-
     //create function to fetch data
     async function Getdata() {
 
@@ -15,31 +13,27 @@
 
         //convert it to json format
         data = await response.json();
-        console.log(data);
-        //loop through heroes to get names
-
-
 
         //get input from id
         let input;
         input = document.getElementById('hero-id').value ;
-        console.log (input );
+        //console.log (input );
 
         //match input id to id heroes
         //console.log(data.heroes[input])
 
-
-        let x = input -1;
+        let x = input ;
         let name = data.heroes[x].name;
         let alterEgo = data.heroes[x].alterEgo;
         let abilities = data.heroes[x].abilities;
         let id = data.heroes[x].id;
+        //console.log(name,alterEgo,abilities);
 
+        //not === cuz id and input not same type
+        data.heroes.splice(data.heroes.findIndex(function split(hero){
+            return hero.id == x;}),1);
 
-        //removes object that has id of X
-        data.heroes.splice(x,1);
-
-       console.log(name,alterEgo,abilities);
+        console.log(data);
 
     }
 
