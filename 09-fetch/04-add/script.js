0471a1a0a7f0b3aa91b0ae2ba18df297eef12a54
imane <benbenimane@hotmail.com>
2020-01-09 16:54:20 +0100

    now i get it

--- 09-fetch/04-add/script.js
+++ 09-fetch/04-add/script.js
@@ -1,50 +1,40 @@
 
 (() => {
+//create function to fetch data
+    function Person(Name, Ego, Powers){
+        this.names = Name;
+        this.egos = Ego;
+        this.abilities = Powers;
+
+    }
 
 
     //create var for URL
     const Url = 'http://localhost:5000/_shared/api.json';
 
+
     //on click run getdata
-    document.getElementById("run").onclick = function() {Getdata()};
-
-
-
-       //create function to fetch data
-       async function Getdata() {
-
-          //fetch stream of data
-          let response = await fetch(Url);
-
-          //convert it to json format
-          let data = await response.json();
-          console.log(data);
+    document.getElementById("run").onclick = function() {createHero()};
+    //create template for object
+    function createHero() {
 
 
           //create array for abilities
-          let powers =[];
+          let Powers1 =[];
+
           //get input
-          let name = document.getElementById('hero-name').value;
-          let ego = document.getElementById('hero-alter-ego').value;
-          let abilities= document.getElementById('hero-powers').value;
+          let names1 = document.getElementById('hero-name').value;
+          let egos1 = document.getElementById('hero-alter-ego').value;
+          let abilities1= document.getElementById('hero-powers').value;
 
           //push abilities in powers
-          powers.push(abilities);
+          Powers1.push(abilities1);
 
-          //console.log(name, ego, powers);
-
-          //create template for object
-           function Person(names, ego, powers){
-               this.names = name;
-               this.egos = ego;
-               this.powers = abilities;
-
-           }
-
+          console.log(Powers1);
 
 
            //var where hero object will be created
-           let hero = new Person(name,ego,abilities);
+           let hero = new Person(names1,egos1,Powers1);
            console.log(hero);
 
 
