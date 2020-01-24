3ecb7389e9018cb731b988ce2e0e9806a576f29f
imane <benbenimane@hotmail.com>
2020-01-24 16:25:40 +0100

    sorted code, understanding sort function

--- 02-maths/03-even-square/script.js
+++ 02-maths/03-even-square/script.js
@@ -4,7 +4,7 @@
     document.getElementById("run").addEventListener("click", function() {
 //a sqaure of a number is the number to the power of 2
       let square;
-      
+
       for (n =1; n < 22; n ++){
       square = Math.pow( n,2);
       alert (square + " ");
--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -1,19 +1,14 @@
-
 //first line is for the button with "run id. it listens to a click, then runs function
 document.getElementById("run").addEventListener("click", function() {
 
 // to get the value of an input: document.getElementById("element-id").value
-// put value into variable arr
-let arr = document.getElementById("numbers").value;
-//the value I get is a string
+// put value into variable
+let numbers = document.getElementById("numbers").value;
 
-
-// split tot transform into arrays, we remove comma and space from string to keep only numbers
-let result = arr.split(", ");
-
+// split tot transform into arrays, we remove comma and space from string to keep only numbers, now we have an array of numbers that we can sort
+let result = numbers.split(", ");
 
 result.sort(function(a,b){return a-b});
-
 
 // after resturning sor, i asign res to "numbers"
 document.getElementById("numbers").value = result;
