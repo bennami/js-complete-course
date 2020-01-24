4fa36a08f02cbb96a6c424ae48d7d030a21b0ad7
imane <benbenimane@hotmail.com>
2020-01-24 15:47:18 +0100

    auto indent

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -3,33 +3,33 @@
     // to get the value of an input: document.getElementById("element-id").value
     let number1,number2;
 
-document.getElementById("addition").addEventListener("click", function() {
-   number1 = document.getElementById ("op-one" ).value;
-    number2 = document.getElementById ("op-two" ).value ;
-   let sum = parseInt(number1) + parseInt(number2);
-    alert (sum);
+    document.getElementById("addition").addEventListener("click", function() {
+        number1 = document.getElementById ("op-one" ).value;
+        number2 = document.getElementById ("op-two" ).value ;
+        let sum = parseInt(number1) + parseInt(number2);
+        alert (sum);
 
     });
 
-document.getElementById("substraction").addEventListener("click", function() {
-    number1 = document.getElementById ("op-one" ).value;
-    number2 = document.getElementById ("op-two" ).value ;
-       let sum = parseInt(number1) - parseInt(number2);
+    document.getElementById("substraction").addEventListener("click", function() {
+        number1 = document.getElementById ("op-one" ).value;
+        number2 = document.getElementById ("op-two" ).value ;
+        let sum = parseInt(number1) - parseInt(number2);
         alert (sum);
     });
 
-document.getElementById("multiplication").addEventListener("click", function() {
-    number1 = document.getElementById ("op-one" ).value;
-    number2 = document.getElementById ("op-two" ).value ;
+    document.getElementById("multiplication").addEventListener("click", function() {
+        number1 = document.getElementById ("op-one" ).value;
+        number2 = document.getElementById ("op-two" ).value ;
 
-      let  sum = parseInt(number1) * parseInt(number2);
+        let  sum = parseInt(number1) * parseInt(number2);
         alert (sum);
     });
 
-document.getElementById("division").addEventListener("click", function() {
-    number1 = document.getElementById ("op-one" ).value;
-    number2 = document.getElementById ("op-two" ).value ;
-      let sum = parseInt(number1) / parseInt(number2);
+    document.getElementById("division").addEventListener("click", function() {
+        number1 = document.getElementById ("op-one" ).value;
+        number2 = document.getElementById ("op-two" ).value ;
+        let sum = parseInt(number1) / parseInt(number2);
         alert (sum);
 
     });
