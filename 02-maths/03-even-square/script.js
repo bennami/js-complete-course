3999973be27c4db6ded121763e8a110d706e1637
imane <benbenimane@hotmail.com>
2020-01-24 16:13:55 +0100

    adding comment square of number

--- 02-maths/03-even-square/script.js
+++ 02-maths/03-even-square/script.js
@@ -2,15 +2,12 @@
 (function() {
 
     document.getElementById("run").addEventListener("click", function() {
-
+//a sqaure of a number is the number to the power of 2
       let square;
-
+      
       for (n =1; n < 22; n ++){
-
       square = Math.pow( n,2);
-
       alert (square + " ");
-
       }
 
     });
